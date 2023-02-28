3347ed15ad1541e008c5dcf19fa3436797332f7a
vittoriaromano <vittoria5005@gmail.com>
2023-02-28 20:02:51 +0400

    lista-spesa

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 'use strict'
+
+//lista spesa
+
+const shoppingList = [
+    'vino',
+    'latte',
+    'cereali',
+    'caffe' 
+];
+
+
+const ul = document.getElementById('shopping-list');
+console.log(ul);
+
+//diamo al'index una variabile precedente al ciclo
+let i = 0; 
+
+
+while(i < shoppingList.length){
+    console.log(shoppingList[i]);
+    const listItem = document.createElement('li');
+    listItem.append(shoppingList[i]);
+    ul.append(listItem);
+    i++;
+}
--- readme.md
+++ readme.md
@@ -3,8 +3,12 @@ nome repo di oggi: js-lista-spesa
 
 //prendiamo l'esercizio for
 
-//diamo al'index e gli diamo una variabile precedente al ciclo
+//diamo al'index una variabile precedente al ciclo
 
 //inseriamo l'index crescente (i++) di uno all'interno del ciclo
 
 //infine convertiamo for con while
+
+
+
+
